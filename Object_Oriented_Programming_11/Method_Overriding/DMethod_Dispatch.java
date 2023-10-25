13194d62f676ddf93fdbe05b8ef31973b45a04a7
Abdullah-Niaz <abdullahniaz423@gmail.com>
2023-10-25 22:04:12 +0500

    Dynamic Method Dispatching

---
+++ Object_Oriented_Programming_11/Method_Overriding/DMethod_Dispatch.java
@@ -0,0 +1,29 @@
+package Object_Oriented_Programming_11.Method_Overriding;
+
+class top{
+    public void display1(){
+        System.out.println("Super Display 1");
+    }
+    public void display2(){
+        System.out.println("Super Display 1");
+    }
+}
+
+class bottom extends top{
+    public void display2(){
+        System.out.println("Super Display 1");
+    }
+    
+    // it can't be displayed,
+    // Throws an undefined Error
+    public void display3(){
+        System.out.println("Super Display 1");
+    }
+}
+
+public class DMethod_Dispatch {
+    public static void main(String[] args) {
+        top t = new bottom();
+        t.display2();
+    }
+}
