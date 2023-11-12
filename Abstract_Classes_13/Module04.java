bc5798d1f74970dd1562df62e5acc032780cb3a2
Abdullah Niaz <abdullahniaz423@gmail.com>
2023-11-12 23:23:54 +0500

    Module 4 Example of Shape containing Hierachy of Circle and Cylinder

---
+++ Abstract_Classes_13/Module04.java
@@ -0,0 +1,14 @@
+package Abstract_Classes_13;
+
+class one{
+    protected int x = 20; 
+}
+class two extends one{
+    two(){
+        System.out.println(Super.x);
+    }
+}
+
+public class Module04 {
+    
+}
